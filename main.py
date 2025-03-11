40868ea56dc7eb388388905528a5061f03da1610
Richard9235 <richardllmajarucon@gmail.com>
2025-03-12 01:27:38 +0800

    B: Added Movement

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x,y)
+    clock = pygame.time.Clock()
+    dt = 0
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color=(0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        player.update(dt)
 
 
 if __name__ == "__main__":
