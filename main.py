0d136553f6cd46a0443837fa908013884399d9e9
Richard9235 <richardllmajarucon@gmail.com>
2025-03-12 01:48:45 +0800

    C: Grouping Player classes, I need to review this

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from constants import *
 from player import *
 
 def main():
+    updatable  = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
-    player = Player(x,y)
+    Player(x,y)
     clock = pygame.time.Clock()
     dt = 0
     while(True):
@@ -15,10 +18,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color=(0,0,0))
-        player.draw(screen)
+        for i in drawable:
+            i.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        player.update(dt)
+        updatable.update(dt)
 
 
 if __name__ == "__main__":
